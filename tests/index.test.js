6b12e340130682dd91604f73bc4686616c81e14f
Fabiano Pires <mineot@gmail.com>
2022-08-05 01:15:22 -0300

    finish invalids tests

--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-import { Dimensions, GenPassword, Types } from "./../src";
-
-test("Index - Invalid Type", async () => {
-  await expect(() => GenPassword(999, 999)).rejects.toEqual("GenPassword Error: Invalid Type");
-});
-
-test("Index - Invalid Dimension", async () => {
-  expect(() => GenPassword(Types.ONLY_NUMBERS, 999)).rejects.toEqual("GenPassword Error: Invalid Dimension");
-});
