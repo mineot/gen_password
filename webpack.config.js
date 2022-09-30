37433cde7828b53945d06417c8d5a6c715ddf229
Fabiano Pires <mineot@gmail.com>
2022-09-29 22:45:53 -0300

    some configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 const path = require("path");
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: path.resolve(__dirname, "src/index.js"),
+  target: "web",
+  mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "gen_password.js",
-    library: {
-      name: "gen_password",
-      type: "umd",
+    library: "$mineot.gen_password",
+    libraryTarget: "umd",
+    globalObject: "this",
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
   devtool: "source-map",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
-    },
-  },
 };
