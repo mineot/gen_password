84bf1a91f174eeb73139c1dae1e0b550ea79d080
Fabiano Pires <fabiano.pires>
2022-11-29 09:34:35 -0300

    config type dimension test

--- tests/core-type-dimension.test.js
+++ tests/core-type-dimension.test.js
@@ -1,14 +1,18 @@
 // @ts-check
 
 import { DimensionEnum } from "@enums/dimension.enum";
+import { TypeDimension } from "@core/type-dimension";
 import { TypeEnum } from "@enums/type.enums";
 
-const typeKeys = ["ONLY_NUMBERS", "ONLY_ALPHAS", "ALPHA_NUMERICS", "COMPLEX"];
-const typeVals = ["only_numbers", "only_alphas", "alpha_numerics", "complex"];
-
-const dimensionKeys = ["FOUR", "SIX", "EIGTH", "TEN", "TWELVE", "FOURTEEN", "SIXTEEN", "EIGHTEEN", "TWENTY"];
-const dimensionVals = [4, 6, 8, 10, 12, 14, 16, 18, 20];
+const typeKeys = Object.values(TypeEnum);
+const dimensionKeys = Object.values(DimensionEnum).map(el => el.toString());
 
 test("Core Type Dimension - Validate Key's", () => {
-  // validar o core type dimension
+  Object.keys(TypeDimension).forEach(typeKey => {
+    expect(typeKeys.includes(typeKey)).toEqual(true);
+
+    Object.keys(TypeDimension[typeKey]).forEach(dimensionKey => {
+      expect(dimensionKeys.includes(dimensionKey)).toEqual(true);
+    });
+  });
 });
